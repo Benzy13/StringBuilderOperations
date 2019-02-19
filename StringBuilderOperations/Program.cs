922ea738d61abaa015bccb8bc2dcdef34aac09af
Jean <Jean@CRONUS>
2019-02-19 17:10:56 +0200

    1st

--- StringBuilderOperations/Program.cs
+++ StringBuilderOperations/Program.cs
@@ -10,6 +10,18 @@ namespace StringBuilderOperations
     {
         static void Main(string[] args)
         {
+            var builder = new StringBuilder();
+            builder
+                .Append('-',10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-','+')
+                .Remove(0, 10)
+                .Insert(0, new string('-',10));
+
+            Console.WriteLine(builder);
         }
     }
 }
